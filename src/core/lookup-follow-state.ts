4ec1a8a6ca0755ff74a7ed4e79badff302aa22ea
syuilo <syuilotan@yahoo.co.jp>
2016-07-10 21:08:40 +0900

    Refactoring

--- src/core/lookup-follow-state.ts
+++ src/core/lookup-follow-state.ts
@@ -1,17 +1,19 @@
 import {Following} from '../db/db';
-import {IFollowing} from '../db/interfaces';
 
-export default function(meId: string, otherpartyId: string): Promise<boolean> {
-	return new Promise<boolean>((resolve, reject) => {
-		UserFollowing.findOne({
-			followee: otherpartyId,
-			follower: meId
-		}, (followingFindErr: any, userFollowing: IUserFollowing) => {
-			if (followingFindErr !== null) {
-				reject(followingFindErr);
-			} else {
-				resolve(userFollowing !== null);
-			}
-		});
+export default (meId: string, otherpartyId: string) =>
+	new Promise<boolean>((resolve, reject) =>
+{
+	Following
+	.count({
+		followee: otherpartyId,
+		follower: meId
+	})
+	.limit(1)
+	.exec((err, exist) => {
+		if (err) {
+			reject(err);
+		} else {
+			resolve(exist === 1);
+		}
 	});
-}
+});
