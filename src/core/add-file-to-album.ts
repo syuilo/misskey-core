7db1783723c7696e73db497c70332723ae5121ac
syuilo <syuilotan@yahoo.co.jp>
2016-07-10 20:58:10 +0900

    Improve readavility

--- src/core/add-file-to-album.ts
+++ src/core/add-file-to-album.ts
@@ -16,7 +16,7 @@ const mimeType = require('mime-types');
  * @param force trueに設定すると、ハッシュが同じファイルが見つかった場合でも無視してアルバムに登録します
  * @return 追加したファイルオブジェクト
  */
-export default function(
+export default (
 	userId: string,
 	fileName: string,
 	file: Buffer,
@@ -24,7 +24,8 @@ export default function(
 	type: string = null,
 	folderId: string = null,
 	force: boolean = false
-): Promise<IAlbumFile> {
+) => new Promise<IAlbumFile>((resolve, reject) =>
+{
 	// ファイルサイズ
 	const size = file.byteLength;
 
@@ -37,116 +38,114 @@ export default function(
 		.update(file)
 		.digest('hex');
 
-	return new Promise<IAlbumFile>((resolve, reject) => {
-		if (!force) {
-			// 同じハッシュ(と同じファイルサイズ(念のため))を持つファイルが既に存在するか確認
-			AlbumFile.findOne({
-				user: userId,
-				hash: hash,
-				datasize: size
-			}, (hashmuchFileFindErr: any, hashmuchFile: IAlbumFile) => {
-				if (hashmuchFileFindErr !== null) {
-					console.error(hashmuchFileFindErr);
-					return reject('something-happend');
-				}
+	if (!force) {
+		// 同じハッシュ(と同じファイルサイズ(念のため))を持つファイルが既に存在するか確認
+		AlbumFile.findOne({
+			user: userId,
+			hash: hash,
+			datasize: size
+		}, (hashmuchFileFindErr: any, hashmuchFile: IAlbumFile) => {
+			if (hashmuchFileFindErr !== null) {
+				console.error(hashmuchFileFindErr);
+				return reject('something-happend');
+			}
 
-				// 無かったら新規登録
-				if (hashmuchFile === null) {
-					register();
-				} else {
-					// あったら登録せずにそれを返却
-					resolve(hashmuchFile);
-				}
-			});
-		} else {
-			// forceがtrueの場合は強制登録
-			register();
-		}
+			// 無かったら新規登録
+			if (hashmuchFile === null) {
+				register();
+			} else {
+				// あったら登録せずにそれを返却
+				resolve(hashmuchFile);
+			}
+		});
+	} else {
+		// forceがtrueの場合は強制登録
+		register();
+	}
 
-		function register(): void {
-			// アルバム使用量を取得するためにすべてのファイルを取得
-			AlbumFile
-			.find({user: userId})
-			.select({
-				datasize: 1,
-				_id: 0
-			})
-			.lean()
-			.exec((albumFilesFindErr: any, albumFiles: IAlbumFile[]) => {
-				if (albumFilesFindErr !== null) {
-					return reject(albumFilesFindErr);
-				}
+	function register(): void {
+		// アルバム使用量を取得するためにすべてのファイルを取得
+		AlbumFile
+		.find({user: userId})
+		.select({
+			datasize: 1,
+			_id: 0
+		})
+		.lean()
+		.exec((albumFilesFindErr: any, albumFiles: IAlbumFile[]) => {
+			if (albumFilesFindErr !== null) {
+				return reject(albumFilesFindErr);
+			}
 
-				// 現時点でのアルバム使用量を算出(byte)
-				const used = albumFiles.map(albumFile => albumFile.datasize).reduce((x, y) => x + y, 0);
+			// 現時点でのアルバム使用量を算出(byte)
+			const used = albumFiles.map(albumFile => albumFile.datasize).reduce((x, y) => x + y, 0);
 
-				// 1GBを超える場合
-				if (used + size > 1073741824) {
-					return reject('no-free-space');
-				}
+			// 1GBを超える場合
+			if (used + size > 1073741824) {
+				return reject('no-free-space');
+			}
 
-				if (folderId !== null) {
-					AlbumFolder.findById(folderId, (folderFindErr: any, folder: IAlbumFolder) => {
-						if (folderFindErr !== null) {
-							return reject(folderFindErr);
-						} else if (folder === null) {
-							return reject('folder-not-found');
-						} else if (folder.user.toString() !== userId) {
-							return reject('folder-not-found');
-						}
-						create(folder);
-					});
-				} else {
-					create(null);
-				}
+			if (folderId !== null) {
+				AlbumFolder.findById(folderId, (folderFindErr: any, folder: IAlbumFolder) => {
+					if (folderFindErr !== null) {
+						return reject(folderFindErr);
+					} else if (folder === null) {
+						return reject('folder-not-found');
+					} else if (folder.user.toString() !== userId) {
+						return reject('folder-not-found');
+					}
+					create(folder);
+				});
+			} else {
+				create(null);
+			}
 
-				function create(folder: IAlbumFolder = null): void {
-					// AlbumFileドキュメントを作成
-					AlbumFile.create({
-						user: userId,
-						folder: folder !== null ? folder.id : null,
-						datasize: size,
-						type: type,
-						name: fileName,
-						comment: comment,
-						hash: hash
-					}, (albumFileCreateErr: any, albumFile: IAlbumFile) => {
-						if (albumFileCreateErr !== null) {
-							console.error(albumFileCreateErr);
-							return reject(albumFileCreateErr);
-						}
+			function create(folder: IAlbumFolder = null): void {
+				// AlbumFileドキュメントを作成
+				AlbumFile.create({
+					user: userId,
+					folder: folder !== null ? folder.id : null,
+					datasize: size,
+					type: type,
+					name: fileName,
+					comment: comment,
+					hash: hash
+				}, (albumFileCreateErr: any, albumFile: IAlbumFile) => {
+					if (albumFileCreateErr !== null) {
+						console.error(albumFileCreateErr);
+						return reject(albumFileCreateErr);
+					}
 
-						// 画像だった場合
-						if (/^image\/.*$/.test(type)) {
-							// 幅と高さを取得してプロパティに保存しておく
-							(<any>gm)(file, fileName)
-							.size((getSizeErr: any, whsize: any) => {
-								if (getSizeErr !== undefined && getSizeErr !== null) {
-									console.error(getSizeErr);
-									return save(albumFile);
-								}
-								albumFile.properties = {
-									width: whsize.width,
-									height: whsize.height
-								};
-								save(albumFile);
-							});
-						} else {
+					// 画像だった場合
+					if (/^image\/.*$/.test(type)) {
+						// 幅と高さを取得してプロパティに保存しておく
+						(<any>gm)(file, fileName)
+						.size((getSizeErr: any, whsize: any) => {
+							if (getSizeErr !== undefined && getSizeErr !== null) {
+								console.error(getSizeErr);
+								return save(albumFile);
+							}
+							albumFile.properties = {
+								width: whsize.width,
+								height: whsize.height
+							};
 							save(albumFile);
-						}
-					});
-				}
-			});
-		}
+						});
+					} else {
+						save(albumFile);
+					}
+				});
+			}
+		});
+	}
 
-		function save(albumFile: IAlbumFile): void {
-			albumFile.save((saveErr: any, saved: IAlbumFile) => {
-				if (saveErr !== null) {
-					console.error(saveErr);
-					return reject(saveErr);
-				}
-				resolve(saved);
-			});
-		}
-	});
-}
+	function save(albumFile: IAlbumFile): void {
+		albumFile.save((saveErr: any, saved: IAlbumFile) => {
+			if (saveErr !== null) {
+				console.error(saveErr);
+				return reject(saveErr);
+			}
+			resolve(saved);
+		});
+	}
+});
