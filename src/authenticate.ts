b4381f518d38346ab217856bd23c17cd2352a0ee
syuilo <syuilotan@yahoo.co.jp>
2016-07-16 02:16:05 +0900

    :v:

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -5,17 +5,19 @@ import config from './config';
 export default (req: express.Request) =>
 	new Promise<{ app: any, user: any, isOfficial: boolean }>(async (resolve, reject) =>
 {
-	if (req.headers['pass'] === undefined || req.headers['pass'] === null) {
+	if (!req.headers['pass']) {
 		resolve({ app: null, user: null, isOfficial: false });
 	} else if (req.headers['pass'] !== config.apiPass) {
 		reject('incorrect-pass');
-	} else if (req.headers['user'] === undefined || req.headers['user'] === null || req.headers['user'] === 'null') {
+	} else if (!req.headers['user'] || req.headers['user'] === 'null') {
 		resolve({ app: null, user: null, isOfficial: true });
 	} else {
 		const user = await User
 			.findById(req.headers['user'])
 			.lean()
 			.exec();
+
+		user.id = user._id;
 
 		resolve({
 			app: null,
