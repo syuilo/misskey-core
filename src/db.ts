b363c2a6dd0a5003acca895a1ce03f226f312f4e
syuilo <syuilotan@yahoo.co.jp>
2016-07-17 03:39:31 +0900

    Fix bug

--- src/db.ts
+++ src/db.ts
@@ -10,7 +10,9 @@ const client = mongodb.MongoClient;
 client.connect(config.mongo.uri, config.mongo.options, (err, db) => {
 	if (err) {
 		console.log(err);
+		return;
 	}
+
 	console.log('Connected to MongoDB');
 
 	(<any>global).db = db;
