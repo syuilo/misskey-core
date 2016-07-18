660a6334559ee29c95a3721250a191b9911fb912
syuilo <syuilotan@yahoo.co.jp>
2016-07-19 01:39:54 +0900

    :v:

--- src/endpoints/signin.js
+++ src/endpoints/signin.js
@@ -15,31 +15,31 @@ import serialize from '../serializers/user';
  * @param {Object} app
  * @return {void}
  */
-module.exports = async (params: any, reply: any, app: any) =>
+module.exports = async (params, reply, app) =>
 {
 	// Init 'username' parameter
 	const username = params.username;
 	if (username === undefined || username === null || username === '') {
-		return reply(400, 'username-is-required');
+		return reply(400, 'username is required', 'INVALID_QUERY');
 	}
 
 	// Init 'password' parameter
 	const password = params.password;
 	if (password === undefined || password === null || password === '') {
-		return reply(400, 'password-is-required');
+		return reply(400, 'password is required', 'INVALID_QUERY');
 	}
 
 	const user = await User.findOne({username});
 
 	if (user === null) {
-		return reply(404, 'user-not-found');
+		return reply(404, 'user not found', 'USER_NOT_FOUND');
 	}
 
 	bcrypt.compare(password, user.password, async (compareErr, same) => {
 		if (compareErr) {
-			return reply(500, 'something-happened');
+			return reply(500);
 		} else if (!same) {
-			return reply(400, 'incorrect-password');
+			return reply(400, 'incorrect password', 'INCORRECT_PASSWORD');
 		}
 
 		reply(await serialize(user));
