e9f0b13d437563323afdedb4d2756721599916aa
syuilo <syuilotan@yahoo.co.jp>
2016-08-30 10:57:02 +0900

    :coffee:

--- src/endpoints/signin.js
+++ src/endpoints/signin.js
@@ -5,7 +5,9 @@
  */
 import * as bcrypt from 'bcrypt';
 import User from '../models/user';
+import Signin from '../models/signin';
 import serialize from '../serializers/user';
+import event from '../event';
 
 /**
  * Signin
@@ -22,26 +24,26 @@ module.exports = async (params, reply, _1, _2, isWeb) =>
 	// Init 'username' parameter
 	const username = params.username;
 	if (username === undefined || username === null || username === '') {
-		return reply(400, 'username is required', 'INVALID_QUERY');
+		return reply(400, 'username is required');
 	}
 
 	// Init 'password' parameter
 	const password = params.password;
 	if (password === undefined || password === null || password === '') {
-		return reply(400, 'password is required', 'INVALID_QUERY');
+		return reply(400, 'password is required');
 	}
 
 	const user = await User.findOne({ username });
 
 	if (user === null) {
-		return reply(404, 'user not found', 'USER_NOT_FOUND');
+		return reply(404, 'user not found');
 	}
 
 	bcrypt.compare(password, user.password, async (compareErr, same) => {
 		if (compareErr) {
 			return reply(500);
 		} else if (!same) {
-			return reply(400, 'incorrect password', 'INCORRECT_PASSWORD');
+			return reply(400, 'incorrect password');
 		}
 
 		reply(await serialize(user, {
@@ -49,5 +51,12 @@ module.exports = async (params, reply, _1, _2, isWeb) =>
 			includeSecrets: isWeb,
 			includeProfileImageIds: isWeb
 		}));
+
+		Signin.insert({
+			created_at: Date.now(),
+			user: user._id
+		});
+
+		//event.
 	});
 };
