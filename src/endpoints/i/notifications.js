c34b2dcaf452c30adc98e63b8c85f12f1bcf7355
syuilo <syuilotan@yahoo.co.jp>
2016-09-04 16:02:32 +0900

    Implement i/notifications

---
+++ src/endpoints/i/notifications.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+import * as mongo from 'mongodb';
+import Notification from '../../models/notification';
+import serialize from '../../serializers/notification';
+
+/**
+ * Get notifications
+ *
+ * @param {Object} params
+ * @param {Object} reply
+ * @param {Object} user
+ * @return {void}
+ */
+module.exports = async (params, reply, user) =>
+{
+	// Init 'limit' parameter
+	let limit = params.limit;
+	if (limit !== undefined && limit !== null) {
+		limit = parseInt(limit, 10);
+
+		// 1 ~ 100 まで
+		if (limit < 1) {
+			return reply(400, 'invalid limit range');
+		} else if (limit > 100) {
+			return reply(400, 'invalid limit range');
+		}
+	} else {
+		limit = 10;
+	}
+
+	const since = params.since || null;
+	const max = params.max || null;
+
+	// 両方指定してたらエラー
+	if (since !== null && max !== null) {
+		return reply(400, 'cannot set since and max');
+	}
+
+	const query = {
+		user: user._id
+	};
+
+	const sort = {
+		created_at: -1
+	};
+
+	if (since !== null) {
+		sort.created_at = 1;
+		query._id = {
+			$gt: new mongo.ObjectID(since)
+		};
+	} else if (max !== null) {
+		query._id = {
+			$lt: new mongo.ObjectID(max)
+		};
+	}
+
+	// クエリ発行
+	const notifications = await Notification
+		.find(query, {}, {
+			limit: limit,
+			sort: sort
+		})
+		.toArray();
+
+	if (notifications.length === 0) {
+		return reply([]);
+	}
+
+	// serialize
+	reply(await Promise.all(notifications.map(async notification => await serialize(notification))));
+};
