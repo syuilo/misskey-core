8a4c757f51e2b2e7a810d433d4b6f0571f062f9c
otofune <otofune@gmail.com>
2016-09-14 12:38:11 +0900

    mongo: with no authorization

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -2,6 +2,10 @@ import * as mongodb from 'mongodb';
 import { IConfig } from '../iconfig';
 
 export default async function(config: IConfig): Promise<mongodb.Db> {
-	const uri = `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+	if (config.mongodb.user && config.mongodb.pass) {
+		const uri = `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+		return await mongodb.MongoClient.connect(uri);
+	}
+	const uri = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
 	return await mongodb.MongoClient.connect(uri);
 };
