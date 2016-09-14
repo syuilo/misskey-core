3fb41b7ff69a8a132c409fbe93e6ca2e4703ad24
otofune <otofune@gmail.com>
2016-09-14 13:52:59 +0900

    mongo: fix

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -3,7 +3,7 @@ import { IConfig } from '../iconfig';
 
 export default async function(config: IConfig): Promise<mongodb.Db> {
 	const uri = config.mongodb.user && config.mongodb.pass
-		? `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+		? `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`
 		: `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
 	return await mongodb.MongoClient.connect(uri);
 };
