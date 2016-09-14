2ecf3cd04cc371c3490d189792098a842a49df15
otofune <otofune@gmail.com>
2016-09-14 13:46:01 +0900

    mongo: use ternary operator

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -2,10 +2,8 @@ import * as mongodb from 'mongodb';
 import { IConfig } from '../iconfig';
 
 export default async function(config: IConfig): Promise<mongodb.Db> {
-	if (config.mongodb.user && config.mongodb.pass) {
-		const uri = `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
-		return await mongodb.MongoClient.connect(uri);
-	}
-	const uri = `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+	const uri = config.mongodb.user && config.mongodb.pass
+		? `mongodb://${config.mongodb.user}:${config.mongodb.pass}@${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
+		: `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.db}`;
 	return await mongodb.MongoClient.connect(uri);
 };
