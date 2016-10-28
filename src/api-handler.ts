104a3260c441e658efeeddaeefa178670fc9367c
syuilo <syuilotan@yahoo.co.jp>
2016-10-29 02:21:32 +0900

    Refactoring :star:

--- src/api-handler.ts
+++ src/api-handler.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
-import * as Limiter from 'ratelimiter';
 import authenticate from './authenticate';
-import limiterDB from './db/redis';
+import limitter from './limitter';
 
 const env = process.env.NODE_ENV;
 const isProduction = env === 'production';
@@ -12,10 +11,6 @@ export default (endpoint: any, req: express.Request, res: express.Response) =>
 	if (isDebug) {
 		console.log(`REQ: ${endpoint.name}`);
 	}
-
-	const limitKey = endpoint.hasOwnProperty('limitKey')
-		? endpoint.limitKey
-		: endpoint.name;
 
 	function reply(x?: any, y?: any): void {
 		if (x === undefined) {
@@ -47,50 +42,6 @@ export default (endpoint: any, req: express.Request, res: express.Response) =>
 		}
 	}
 
-	// 短い期間の方のリミット
-	function detectBriefInterval(ctx: any): void {
-		const minIntervalLimiter = new Limiter({
-			id: `${ctx.user._id}:${limitKey}:for-detect-brief-interval`,
-			duration: endpoint.minInterval,
-			max: 1,
-			db: limiterDB
-		});
-
-		minIntervalLimiter.get((limitErr, limit) => {
-			if (limitErr !== null) {
-				reply(500);
-			} else if (limit.remaining === 0) {
-				reply(429, 'BRIEF_REQUEST_INTERVAL');
-			} else {
-				if (endpoint.hasOwnProperty('limitDuration') && endpoint.hasOwnProperty('limitMax')) {
-					rateLimit(ctx);
-				} else {
-					call(ctx);
-				}
-			}
-		});
-	}
-
-	// 長い期間の方のリミット
-	function rateLimit(ctx: any): void {
-		const limiter = new Limiter({
-			id: `${ctx.user._id}:${limitKey}`,
-			duration: endpoint.limitDuration,
-			max: endpoint.limitMax,
-			db: limiterDB
-		});
-
-		limiter.get((limitErr, limit) => {
-			if (limitErr !== null) {
-				reply(500);
-			} else if (limit.remaining === 0) {
-				reply(429, 'RATE_LIMIT_EXCEEDED');
-			} else {
-				call(ctx);
-			}
-		});
-	}
-
 	function call(ctx: any): void {
 		try {
 			if (endpoint.withFile) {
@@ -116,13 +67,7 @@ export default (endpoint: any, req: express.Request, res: express.Response) =>
 				return reply(401, 'PLZ_SIGNIN');
 			}
 
-			if (endpoint.hasOwnProperty('minInterval')) {
-				detectBriefInterval(ctx);
-			} else if (endpoint.hasOwnProperty('limitDuration') && endpoint.hasOwnProperty('limitMax')) {
-				rateLimit(ctx);
-			} else {
-				call(ctx);
-			}
+			limitter(endpoint, ctx).then(() => call(ctx), err => reply(429));
 		} else {
 			call(ctx);
 		}
