25896ede4309d9a4b3576f92478cb28bbe50b063
syuilo <syuilotan@yahoo.co.jp>
2016-11-01 20:25:37 +0900

    Implement auth/accept

---
+++ src/endpoints/auth/accept.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+import rndstr from 'rndstr';
+import AuthSess from '../../models/auth-session';
+import Userkey from '../../models/userkey';
+
+/**
+ * Accept
+ *
+ * @param {Object} params
+ * @param {Object} user
+ * @return {Promise<object>}
+ */
+module.exports = (params, user) =>
+	new Promise(async (res, rej) =>
+{
+	// Get 'token' parameter
+	const token = params.token;
+	if (token == null) {
+		return rej('token is required');
+	}
+
+	// Fetch token
+	const session = await AuthSess
+		.findOne({ token: token });
+
+	if (session === null) {
+		return rej('session not found');
+	}
+
+	// Generate userkey
+	const key = rndstr('a-zA-Z0-9', 32);
+
+	// Insert userkey doc
+	await Userkey.insert({
+		created_at: new Date(),
+		app: session.app,
+		user: user._id,
+		key: key
+	});
+
+	// Response
+	res();
+}
