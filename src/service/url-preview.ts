0d9e0011869e989001ea58ef70b2ec1d4a277e69
syuilo <syuilotan@yahoo.co.jp>
2016-11-18 07:05:34 +0900

    Add note

--- src/service/url-preview.ts
+++ src/service/url-preview.ts
@@ -2,5 +2,6 @@ import * as express from 'express';
 import summaly from 'summaly';
 
 export default async function (req: express.Request, res: express.Response): Promise<void> {
+	// TODO: Wrap a non-https resources with a proxy
 	res.send(await summaly(req.query.url));
 };
