c6693d778994f539c4dfd47d4dd25d0367f2e917
syuilo <syuilotan@yahoo.co.jp>
2016-12-09 11:27:11 +0900

    制限緩和

--- src/api/models/drive-file.ts
+++ src/api/models/drive-file.ts
@@ -5,7 +5,7 @@ export default ((global as any).db as mongodb.Db).collection('drive_files');
 export function validateFileName(name: string): boolean {
 	return (
 		(name.trim().length > 0) &&
-		(name.length <= 128) &&
+		(name.length <= 200) &&
 		(name.indexOf('\\') === -1) &&
 		(name.indexOf('/') === -1) &&
 		(name.indexOf('..') === -1)
