a0dbe1d53b21ce3b27ed73c16c5ae48659f6503a
syuilo <syuilotan@yahoo.co.jp>
2016-12-12 01:58:09 +0900

    Add api prefix to resolve conflict

--- src/web/server.ts
+++ src/web/server.ts
@@ -44,7 +44,7 @@ app.use('/_/resources', express.static(`${__dirname}/client/resources`, {
 /**
  * Common API
  */
-app.get('/meta',     require('./meta'));
+app.get('/api:meta', require('./meta'));
 app.get('/api:url',  require('./service/url-preview'));
 app.post('/api:rss', require('./service/rss-proxy'));
 
