a51717f6c55dcc51a2dd0e9ef53f1e3788a94cff
syuilo <syuilotan@yahoo.co.jp>
2016-12-13 13:17:23 +0900

    Refactor: Use recaptcha-promise package for verfing

--- src/api/private/signup.ts
+++ src/api/private/signup.ts
@@ -1,105 +1,93 @@
 import * as express from 'express';
-import * as request from 'request';
 import * as bcrypt from 'bcrypt';
 import rndstr from 'rndstr';
+const recaptcha = require('recaptcha-promise');
 import User from '../models/user';
 import { validateUsername } from '../models/user';
 import serialize from '../serializers/user';
 
 import config from '../../config';
 
-export default (req: express.Request, res: express.Response) => {
-	request({
-		url: 'https://www.google.com/recaptcha/api/siteverify',
-		method: 'POST',
-		form: {
-			secret: config.recaptcha.secretKey,
-			response: req.body['g-recaptcha-response']
-		}
-	}, (err, response, body) => {
-		if (err) {
-			console.error(err);
-			res.sendStatus(500);
-			return;
-		}
+recaptcha.init({
+	secret_key: config.recaptcha.secretKey
+});
 
-		const parsed = JSON.parse(body);
+export default async (req: express.Request, res: express.Response) => {
+	const success = await recaptcha(req.body['g-recaptcha-response']);
 
-		if (parsed.success) {
-			signup();
-		} else {
-			res.status(400).send('recaptcha-failed');
-		}
-	});
+	if (!success) {
+		res.status(400).send('recaptcha-failed');
+		return;
+	}
 
-	async function signup(): Promise<any> {
-		const username = req.body['username'];
-		const password = req.body['password'];
-		const name = '名無し';
+	const username = req.body['username'];
+	const password = req.body['password'];
+	const name = '名無し';
 
-		// Validate username
-		if (!validateUsername(username)) {
-			return res.sendStatus(400);
-		}
+	// Validate username
+	if (!validateUsername(username)) {
+		res.sendStatus(400);
+		return;
+	}
 
-		const usernameExist = await User
-			.count({
-				username_lower: username.toLowerCase()
-			}, {
-				limit: 1
-			});
-
-		// Check username already used
-		if (usernameExist !== 0) {
-			return res.sendStatus(400);
-		}
-
-		// Generate hash of password
-		const salt = bcrypt.genSaltSync(14);
-		const hash = bcrypt.hashSync(password, salt);
-
-		// Generate secret
-		const secret = rndstr('a-zA-Z0-9', 32);
-
-		// Create account
-		const inserted = await User.insert({
-			token: secret,
-			avatar_id: null,
-			banner_id: null,
-			birthday: null,
-			created_at: new Date(),
-			bio: null,
-			email: null,
-			followers_count: 0,
-			following_count: 0,
-			links: null,
-			location: null,
-			name: name,
-			password: hash,
-			posts_count: 0,
-			likes_count: 0,
-			liked_count: 0,
-			drive_capacity: 1073741824, // 1GB
-			username: username,
+	const usernameExist = await User
+		.count({
 			username_lower: username.toLowerCase()
+		}, {
+			limit: 1
 		});
 
-		const account = inserted.ops[0];
+	// Check username already used
+	if (usernameExist !== 0) {
+		res.sendStatus(400);
+		return;
+	}
 
-		// Response
-		res.send(await serialize(account));
+	// Generate hash of password
+	const salt = bcrypt.genSaltSync(14);
+	const hash = bcrypt.hashSync(password, salt);
 
-		// Create search index
-		if (config.elasticsearch.enable) {
-			const es = require('../../db/elasticsearch');
-			es.index({
-				index: 'misskey',
-				type: 'user',
-				id: account._id.toString(),
-				body: {
-					username: username
-				}
-			});
-		}
+	// Generate secret
+	const secret = rndstr('a-zA-Z0-9', 32);
+
+	// Create account
+	const inserted = await User.insert({
+		token: secret,
+		avatar_id: null,
+		banner_id: null,
+		birthday: null,
+		created_at: new Date(),
+		bio: null,
+		email: null,
+		followers_count: 0,
+		following_count: 0,
+		links: null,
+		location: null,
+		name: name,
+		password: hash,
+		posts_count: 0,
+		likes_count: 0,
+		liked_count: 0,
+		drive_capacity: 1073741824, // 1GB
+		username: username,
+		username_lower: username.toLowerCase()
+	});
+
+	const account = inserted.ops[0];
+
+	// Response
+	res.send(await serialize(account));
+
+	// Create search index
+	if (config.elasticsearch.enable) {
+		const es = require('../../db/elasticsearch');
+		es.index({
+			index: 'misskey',
+			type: 'user',
+			id: account._id.toString(),
+			body: {
+				username: username
+			}
+		});
 	}
 };
